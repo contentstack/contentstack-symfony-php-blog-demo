2eb3a57be364e50df8a79b2cfe3f7db0109bf7d0
rishirajupadhyay <rishiraj.upadhyay@raweng.com>
2017-10-11 18:46:47 +0530

    Initial commit

---
+++ web/js/scripts.js
@@ -0,0 +1,54 @@
+(function ($) {
+    'use strict';
+
+    jQuery(document).ready(function () {
+
+       /* Preloader */
+		
+        $(window).load(function () {
+            $('.preloader').delay(800).fadeOut('slow');
+         });
+
+       /* Smooth Scroll */
+
+        $('a.smoth-scroll').on("click", function (e) {
+            var anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $(anchor.attr('href')).offset().top - 50
+            }, 1000);
+            e.preventDefault();
+        });
+
+		
+       /* Scroll To Top */
+		
+        $(window).scroll(function(){
+        if ($(this).scrollTop() >= 500) {
+            $('.scroll-to-top').fadeIn();
+         } else {
+            $('.scroll-to-top').fadeOut();
+         }
+         });
+	
+	
+        $('.scroll-to-top').click(function(){
+          $('html, body').animate({scrollTop : 0},800);
+          return false;
+          });
+		  
+
+       /* Load More Post */	
+	   	
+        $("div.blog-post").slice(0, 4).show();
+          $("#load-more-post").on('click', function (e) {
+             e.preventDefault();
+             $("div.blog-post:hidden").slice(0, 1).slideDown(300);
+             if ($("div.blog-post:hidden").length == 0) {
+             $('#post-end-message').html('<div class="end">End</div>').fadeIn(800);
+             $("#load-more-post").fadeOut(100);
+              }
+             });
+              
+            });
+
+   })(jQuery);
